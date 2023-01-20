65d07728e7dd013dd355b0ea5ad0c148e01cae8f
Vik <85831468+viiktr@users.noreply.github.com>
2023-01-20 21:26:13 +0200

    Update main.py

--- wanda/main.py
+++ wanda/main.py
@@ -1,30 +1,41 @@
 # The Wanda Magic teapot virtual version. Just like the original it has 5 options that will guide your future. Just think of a question and see.
 import random
 import time
-(print("Hmmm, the answer to your question is... • Хммм, отговорът на твоя въпрос е:"))
+
+(print(" ⠀⠀           ⢀⣘⣿⣿⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"))
+(print("⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⢀⣀⠘⠛⠛⠛⠛⠛⠛⠁⣀⠀"))
+(print("⠀⢠⡿⠋⠉⠛⠃⣠⣤⣈⣉⡛⠛⠛⠛⠛⠛⠛⢛⣉⣁⣤⣄⠀⠀⣾⣿⡿⠗"))
+(print("⠀⢸⡇⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⣿⣿"))
+(print("⠀⢸⣇⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢉⣉⣠⣿⣿⡀"))
+(print("⠀⠀⠙⠷⡆⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⢰⣿⣿⣿⣿⣿⡇⠀"))
+(print("⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠸⣿⣿⣿⣿⠟⠀"))
+(print("⠀⠀⠀⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠈⠉⠁⠀⠀"))
+(print("⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠙⠻⠿⠿⠿⠿⠿⠿⠛⠋"))
+
+(print("Welcome to the magical teapot!"))
 time.sleep(2)
-print ("
-#                     ____( )_____
-#     ___            |            |     ____
-#     \  \         _---__________---_  / __ \
-#      \  \       /                  \/ /  \ \
-#_______|  |_____/                    \/____\ \_________________________________
-       |  |    /                      \     | |
-        \  \__|                        |    | |      _____________
-         \    |                        |    | |      |           |/\
-          \   |                        |   / /       |___________|  \
-           \__\                        /__/ /        \-----------/  /
-               \                      /____/          |---------|__/
-                \____________________/               / \ _____ / \
-Vicky Wilks      |__________________|                \___________/")
-answer=random.randint(1,5)
-if answer==1:
-    print("Yes • Да")
-elif answer==2:
-    print("No • Не")
-elif answer == 3:
-        print("Maybe • Може би")
-elif answer == 4:
-        print("Not today • Не и днес")
-else:
-        print("Who knows • Кой знае")
+(print("Think of a question..."))
+time.sleep(3)
+
+while True:
+    answer = random.randint(1, 5)
+    (print("Hmmm, the answer to your question is..."))
+    time.sleep(2)
+    if answer == 1:
+        print("The Teapot says... Yes✅")
+    elif answer == 2:
+        print("The Teapot says... No❌")
+    elif answer == 3:
+        print("The Teapot says... Maybe❔")
+    elif answer == 4:
+        print("The Teapot says... Not today🕙")
+    else:
+        print("The Teapot says... Who knows🤔")
+
+    again = input("Do you have another question for the teapot?  Yes/No ")
+
+    if 'yes' in again:
+            continue
+    else:
+            print("See you next time!")
+            break
